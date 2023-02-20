24172dd52c0cb8e07bc9c7e678ac95121617594d
Paola Llerena Schiffmacher <p.llerena91@gmail.com>
2023-02-20 13:05:07 +0100

    rails-stupid-coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @search = params[:answer]
+
+    if @search == 'I am going to work'
+      @answer = 'Great!'
+    elsif @search.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I do not care, get dressed and go to work!'
+    end
+  end
+end
